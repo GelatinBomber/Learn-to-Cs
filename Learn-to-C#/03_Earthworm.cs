8a0a425767234797afcfc8be86ee3a70c5bd0eb9
GelatinBomber <besis001@gmail.com>
2023-07-27 20:34:10 +0900

    숫자 야구 게임 정답 랜덤으로 생성하기

--- Learn-to-C#/03_Earthworm.cs
+++ Learn-to-C#/03_Earthworm.cs
@@ -1,46 +1,46 @@
-﻿public static class Client
-{
-    public static void Main()
-    {
-        int xSize = 20;
-        int ySize = 20;
+﻿//public static class Client
+//{
+//    public static void Main()
+//    {
+//        int xSize = 20;
+//        int ySize = 20;
 
-        while (true)
-        {
+//        while (true)
+//        {
 
-            //상단
-            for (int i = 0; i < xSize; i++)
-            {
-                Console.Write("■");
-            }
-            Console.WriteLine();
+//            //상단
+//            for (int i = 0; i < xSize; i++)
+//            {
+//                Console.Write("■");
+//            }
+//            Console.WriteLine();
 
-            // 세로 벽
-            for (int i = 0;i < ySize-2; i++)
-            {
-                Console.Write("■");
-                for (int k  = 0; k < xSize-2; k++)
-                {
-                    Console.Write("□");
-                }
-                Console.Write("■");
-                Console.WriteLine();
-            }
+//            // 세로 벽
+//            for (int i = 0;i < ySize-2; i++)
+//            {
+//                Console.Write("■");
+//                for (int k  = 0; k < xSize-2; k++)
+//                {
+//                    Console.Write("□");
+//                }
+//                Console.Write("■");
+//                Console.WriteLine();
+//            }
 
-            // 하단
-            for (int i = 0; i < xSize; i++)
-            {
-                Console.Write("■");
-            }
-            Console.WriteLine();
-
-
+//            // 하단
+//            for (int i = 0; i < xSize; i++)
+//            {
+//                Console.Write("■");
+//            }
+//            Console.WriteLine();
 
 
 
 
-            Thread.Sleep(500);
-            Console.Clear();
-        }
-    }
-}
+
+
+//            Thread.Sleep(500);
+//            Console.Clear();
+//        }
+//    }
+//}
--- Learn-to-C#/04_2_NumBaseBallGameClear.cs
+++ Learn-to-C#/04_2_NumBaseBallGameClear.cs
@@ -8,7 +8,7 @@
 
 
 //// 수정 사항
-//    // 기존에 if로 자릿수를 하나하나 체크했는데, List를 이용해서 한번에 해보자.
+//// 기존에 if로 자릿수를 하나하나 체크했는데, List를 이용해서 한번에 해보자.
 
 
 //public static class Client
@@ -40,13 +40,13 @@
 
 //                        List<int> AnswerNumList = new List<int>();  // 각 자릿수를 리스트에 담기
 //                        AnswerNumList.Add(AnswerNum % 10);
-//                        AnswerNumList.Add((AnswerNum % 100 - AnswerNum % 10)/10);
-//                        AnswerNumList.Add((AnswerNum % 1000 - AnswerNum % 100)/100);
+//                        AnswerNumList.Add((AnswerNum % 100 - AnswerNum % 10) / 10);
+//                        AnswerNumList.Add((AnswerNum % 1000 - AnswerNum % 100) / 100);
 
 //                        List<int> QuestionNumList = new List<int>();
 //                        QuestionNumList.Add(QuetionNum % 10);
-//                        QuestionNumList.Add((QuetionNum % 100 - QuetionNum % 10)/10);
-//                        QuestionNumList.Add((QuetionNum % 1000 - QuetionNum % 100)/100);
+//                        QuestionNumList.Add((QuetionNum % 100 - QuetionNum % 10) / 10);
+//                        QuestionNumList.Add((QuetionNum % 1000 - QuetionNum % 100) / 100);
 
 //                        // 스트라이크 개수 세기
 //                        for (int i = 0; i < 3; i++)
@@ -58,9 +58,9 @@
 //                        }
 
 //                        // 볼 개수 세기
-//                        for (int i = 0; i<3; i++)
+//                        for (int i = 0; i < 3; i++)
 //                        {
-//                            for (int j = 0; j<3; j++)
+//                            for (int j = 0; j < 3; j++)
 //                            {
 //                                if (AnswerNumList[i] == QuestionNumList[j])
 //                                {
@@ -88,7 +88,8 @@
 //            {
 //                Console.WriteLine("입력한 데이터가 정수가 아닙니다." +
 //                    "\n3 자리 정수를 입력하세요.");
-//;            }
+//                ;
+//            }
 //        }
 //    }
 //}
