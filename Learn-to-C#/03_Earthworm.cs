1fe2fda3c4958f29f4e8d0195b778e1a85ee8dd8
GelatinBomber <besis001@gmail.com>
2023-07-27 19:14:01 +0900

    지렁이 게임 만들기 시작

--- Learn-to-C#/03_Earthworm.cs
+++ Learn-to-C#/03_Earthworm.cs
@@ -1,18 +1,46 @@
-﻿/*
-public static class Client
+﻿public static class Client
 {
     public static void Main()
     {
-        int xSize = 30;
-        int ySize = 30;
+        int xSize = 20;
+        int ySize = 20;
 
         while (true)
         {
+
+            //상단
             for (int i = 0; i < xSize; i++)
             {
-                Console.Write("▨");
+                Console.Write("■");
             }
+            Console.WriteLine();
+
+            // 세로 벽
+            for (int i = 0;i < ySize-2; i++)
+            {
+                Console.Write("■");
+                for (int k  = 0; k < xSize-2; k++)
+                {
+                    Console.Write("□");
+                }
+                Console.Write("■");
+                Console.WriteLine();
+            }
+
+            // 하단
+            for (int i = 0; i < xSize; i++)
+            {
+                Console.Write("■");
+            }
+            Console.WriteLine();
+
+
+
+
+
+
+            Thread.Sleep(500);
+            Console.Clear();
         }
     }
 }
-*/
